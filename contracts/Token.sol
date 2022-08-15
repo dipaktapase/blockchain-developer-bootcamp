c1f28603b4a90f7c11ccc5b2aed39674180f223f
dipaktapase <dipaktapase1512@gmail.com>
2022-08-15 09:42:02 +0530

    6. Token Approval

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,11 +9,17 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    mapping( address => uint256 ) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed from,
         address indexed to,
+        uint256 value);
+
+    event Approval(
+        address owner,
+        address spender, 
         uint256 value);
 
     constructor(
@@ -36,6 +42,16 @@ contract Token {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) 
+    public 
+    returns(bool success) 
+    {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value; 
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 } 
