e99217670950d04372b10452fab2094f29bb93df
dipaktapase <dipaktapase1512@gmail.com>
2022-08-16 12:37:03 +0530

    10. Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,16 +11,39 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
 
+    // -------------------
+    // DEPOSIT & WITHDRAW TOKEN
+
     function depositToken(address _token, uint256 _amount) public  {
+        // Transfer tokens to exchange
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
+        // Update user balance
         tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+        // Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        // Ensure user has enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);
+
+        // Transfer token to user
+        Token(_token).transfer(msg.sender, _amount);
+
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        // Emit an event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     function balanceOf(address _token, address _user) 
@@ -30,5 +53,5 @@ contract Exchange {
     {
         return tokens[_token][_user];
     }
-
+    
 }
