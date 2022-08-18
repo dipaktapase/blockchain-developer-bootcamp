4d6b937318aa464ad362824740adf21072dfdea6
dipaktapase <dipaktapase1512@gmail.com>
2022-08-18 13:44:54 +0530

    12. Cancel Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,6 +11,7 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order)public orders;
     uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     event Deposit(
         address token, 
@@ -35,8 +36,17 @@ contract Exchange {
         uint256 amountGive,
         uint256 timestamp
         );
-    
 
+    event Cancel(
+        uint256 id, 
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+    
     struct _Order {
         uint256 id; // Unique identifier for order
         address user; // user who made order
@@ -122,8 +132,25 @@ contract Exchange {
             block.timestamp
         );
     }
-    
 
+    function cancelOrder(uint256 _id) public{
+        // Fetch the order
+        _Order storage _order = orders[_id];
+        // Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender);
+        // Order must exist
+        require(_order.id == _id);
+        // cancel the order
+        orderCancelled[_id] = true;
 
-    
-}
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+    }
+} 
