d016161f5ae488c0c9cdbe3c9adb57d166db7993
dipaktapase <dipaktapase1512@gmail.com>
2022-09-04 12:47:36 +0530

    23. Make Orders

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -61,6 +61,11 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on('Withdraw', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS', event })
   })
+
+  exchange.on('Order', (id, user, tokenGet, amountGet, tokenGive, amountGive, timestamp, event) => {
+    const order = event.args
+    dispatch({ type: 'NEW_ORDER_SUCCESS', order, event})
+  })
 }
 
 
@@ -108,4 +113,45 @@ export const transferTokens = async (provider, exchange, transferType, token, am
     dispatch({ type: 'TRANSFER_FAIL' })
   }
   
+}
+
+  // -------------------------------------------------------------------------------
+  //  Orders ( Buy & Sell)
+
+export const makeBuyOrder = async (provider, exchange, tokens, order, dispatch) => {
+
+  const tokenGet = tokens[0].address
+  const amountGet = ethers.utils.parseUnits(order.amount, 18)
+  const tokenGive = tokens[1].address
+  const amountGive = ethers.utils.parseUnits((order.amount * order.price).toString(), 18)
+
+  dispatch ({ type: 'NEW_ORDER_REQUEST' })
+
+  try {
+    const signer = await provider.getSigner()
+    const transaction = await exchange.connect(signer).makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+    await transaction.wait()
+
+   } catch (error) {
+    dispatch({ type: 'NEW_ORDER_FAIL'})
+   } 
+}
+
+export const makeSellOrder = async (provider, exchange, tokens, order, dispatch) => {
+
+  const tokenGet = tokens[1].address
+  const amountGet = ethers.utils.parseUnits((order.amount * order.price).toString(), 18)
+  const tokenGive = tokens[0].address
+  const amountGive = ethers.utils.parseUnits(order.amount, 18)
+
+  dispatch ({ type: 'NEW_ORDER_REQUEST' })
+
+  try {
+    const signer = await provider.getSigner()
+    const transaction = await exchange.connect(signer).makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+    await transaction.wait()
+
+   } catch (error) {
+    dispatch({ type: 'NEW_ORDER_FAIL'})
+   } 
 }
